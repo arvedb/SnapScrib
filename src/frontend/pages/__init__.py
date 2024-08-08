6588bf8880eca8f55f9d5aeb01d3b378f8c03e55
Cipher shadow <mprogrammierer@gmail.com>
2024-08-08 22:00:08 +0200

    Flask SECRET_KEY

--- src/frontend/pages/__init__.py
+++ src/frontend/pages/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask 
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+FLASK_KEY = os.getenv('FLASK_KEY')
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshjdhjs'
-
-    
+    app.config['SECRET_KEY'] = FLASK_KEY
 
     return app
